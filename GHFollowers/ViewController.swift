55d958b53d9657aadeacb6320b2ed7650aa9276f
Billy Chan <billycychan.work@gmail.com>
2024-01-20 17:26:15 -0500

    initial setup

---
+++ GHFollowers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by CHI YU CHAN on 20/1/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemPink
+    }
+
+
+}
+
