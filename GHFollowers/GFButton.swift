cee14302298d0df6fce35a6276c1c1890ff1a0b7
Billy Chan <billycychan.work@gmail.com>
2024-01-20 18:28:21 -0500

    feat: add the custom button and custom textField

---
+++ GHFollowers/GFButton.swift
@@ -0,0 +1,35 @@
+//
+//  GFButton.swift
+//  GHFollowers
+//
+//  Created by CHI YU CHAN on 20/1/2024.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        
+        configure()
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10.0
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
