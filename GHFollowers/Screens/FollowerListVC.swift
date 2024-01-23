0a03484ccb652bc98f81f48d9deb18ff9c7c5109
Billy Chan <billycychan.work@gmail.com>
2024-01-23 15:05:33 -0500

    feat(network): refactor the network layer using Result Type

--- GHFollowers/Screens/FollowerListVC.swift
+++ GHFollowers/Screens/FollowerListVC.swift
@@ -16,12 +16,14 @@ class FollowerListVC: UIViewController {
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
         
-        NetworkManager.shared.getFollowers(for: username, page: 1) { followers, errorMsg in
-            guard let followers = followers else {
+        NetworkManager.shared.getFollowers(for: username, page: 1) { result in
+            switch result {
+            case .success(let followers):
+               fatalError("Implement it")
+            case .failure(let error):
                 self.presentGFAlertOnMainThread(title: "Bad Stuff Happened",
-                                                message: errorMsg?.rawValue ?? "Error",
+                                                message: error.rawValue,
                                                 buttonTitle: "Ok")
-                return
             }
         }
     }
