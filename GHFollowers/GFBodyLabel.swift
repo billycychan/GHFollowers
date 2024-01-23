0288f9e75bcca5ff45a03a0e4434aba23f219704
Billy Chan <billycychan.work@gmail.com>
2024-01-23 00:32:49 -0500

    feat: create a custom alert controller

---
+++ GHFollowers/GFBodyLabel.swift
@@ -0,0 +1,35 @@
+//
+//  GFBodyLabel.swift
+//  GHFollowers
+//
+//  Created by CHI YU CHAN on 22/1/2024.
+//
+
+import UIKit
+
+class GFBodyLabel: UILabel {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(textAlignment: NSTextAlignment) {
+        super.init(frame: .zero)
+        self.textAlignment = textAlignment
+        configure()
+    }
+    
+    private func configure() {
+        textColor = .label
+        font = UIFont.preferredFont(forTextStyle: .body)
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.9
+        lineBreakMode = .byWordWrapping
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
