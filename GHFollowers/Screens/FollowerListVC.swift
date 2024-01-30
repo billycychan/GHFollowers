ee6fedc6783a1e648b09bee5a08983d4da02cc2f
Billy Chan <billycychan.work@gmail.com>
2024-01-30 03:28:41 -0500

    feat: add empty state view for 0 followers

--- GHFollowers/Screens/FollowerListVC.swift
+++ GHFollowers/Screens/FollowerListVC.swift
@@ -59,7 +59,17 @@ class FollowerListVC: UIViewController {
                     self.hasMoreFollower = false
                 }
                 self.followers.append(contentsOf: followers)
+                
+                if self.followers.isEmpty {
+                    let message = "This user doesn't have any followers. Go follow them😀"
+                    DispatchQueue.main.async {
+                        self.showEmptyStateView(with: message, in: self.view)
+                        return
+                    }
+                }
+                
                 self.updateData()
+                
             case .failure(let error):
                 self.presentGFAlertOnMainThread(title: "Bad Stuff Happened",
                                                 message: error.rawValue,
