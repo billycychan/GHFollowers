fdb183316b11ededf163075c52665d58f582b3ad
Billy Chan <billycychan.work@gmail.com>
2024-02-01 00:34:56 -0500

    feat(UserInfo): setup the GFFllowerItemVC and GFItemInfoVC

---
+++ GHFollowers/Custom Views/ViewControllers/ItemInfoVC/GFFollowerItemVC.swift
@@ -0,0 +1,22 @@
+//
+//  GFFollowerItemVC.swift
+//  GHFollowers
+//
+//  Created by CHI YU CHAN on 1/2/2024.
+//
+
+import Foundation
+
+class GFFollowerItemVC: GFItemInfoVC {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureItems()
+    }
+    
+    private func configureItems() {
+        itemInfoViewOne.set(itemInfoType: .followers, withCount: user.followers)
+        itemInfoViewTwo.set(itemInfoType: .following, withCount: user.following)
+        actionButton.set(backgroundColor: .systemGreen, title: "Get Followers")
+    }
+}
---
+++ GHFollowers/Custom Views/ViewControllers/ItemInfoVC/GFReportItemVC.swift
@@ -0,0 +1,22 @@
+//
+//  GFReportItemVC.swift
+//  GHFollowers
+//
+//  Created by CHI YU CHAN on 1/2/2024.
+//
+
+import Foundation
+
+class GFReportItemVC: GFItemInfoVC {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureItems()
+    }
+    
+    private func configureItems() {
+        itemInfoViewOne.set(itemInfoType: .repos, withCount: user.publicRepos)
+        itemInfoViewTwo.set(itemInfoType: .gists, withCount: user.publicGists)
+        actionButton.set(backgroundColor: .systemPurple, title: "GitHub Profile")
+    }
+}
