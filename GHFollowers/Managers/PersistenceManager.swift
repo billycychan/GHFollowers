b1cadf7b4b4014f9b391caa3138ff9ea88f8da77
Billy Chan <billycychan.work@gmail.com>
2024-02-12 16:27:37 -0500

    feat(Persistencce): add PersistenceManager for favorites

---
+++ GHFollowers/Managers/PersistenceManager.swift
@@ -0,0 +1,72 @@
+//
+//  PersistenceManager.swift
+//  GHFollowers
+//
+//  Created by CHI YU CHAN on 12/2/2024.
+//
+
+import Foundation
+
+enum PersistenceActionType {
+    case add, remove
+}
+
+enum PersistenceManager  {
+    static private let defaults = UserDefaults.standard
+    
+    static func updateWith(favorite: Follower, actionType: PersistenceActionType, completion: @escaping (GFError?)-> Void) {
+        retrieveFavorite { result in
+            switch result {
+            case .success(let favorites):
+                var retrievedFavorites = favorites
+                
+                switch actionType {
+                case .add:
+                    guard !retrievedFavorites.contains(favorite) else {
+                        completion(.alreadyInFavorites)
+                        return
+                    }
+                    
+                    retrievedFavorites.append(favorite)
+                    
+                case .remove:
+                    retrievedFavorites.removeAll(where: { $0.login == favorite.login })
+                    completion(save(favorites: retrievedFavorites))
+                }
+                
+            case .failure(let error):
+                completion(error)
+            }
+        }
+    }
+    
+    enum Keys {
+        static let favorites = "favorites"
+    }
+    
+    static func retrieveFavorite(completion: @escaping (Result<[Follower], GFError>) -> Void) {
+        guard let favoritesData = defaults.object(forKey: Keys.favorites) as? Data else {
+            completion(.success([]))
+            return
+        }
+        
+        do {
+            let decoder = JSONDecoder()
+            let favorites = try decoder.decode([Follower].self, from: favoritesData)
+            completion(.success(favorites))
+        } catch {
+            completion(.failure(.unableToFavorite))
+        }
+    }
+    
+    static func save(favorites: [Follower]) -> GFError? {
+        do {
+            let encoder = JSONEncoder()
+            let encodedFavorites = try encoder.encode(favorites)
+            defaults.set(encodedFavorites, forKey: Keys.favorites)
+            return nil
+        } catch {
+            return .unableToFavorite
+        }
+    }
+}
--- GHFollowers/Screens/FollowerListVC.swift
+++ GHFollowers/Screens/FollowerListVC.swift
@@ -45,6 +45,13 @@ class FollowerListVC: UIViewController {
     func configureViewController() {
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButtonTapped))
+        navigationItem.rightBarButtonItem = addButton
+    }
+    
+    @objc func addButtonTapped() {
+        print("Add BUtton tapped")
     }
     
     func configureCollectionView() {
