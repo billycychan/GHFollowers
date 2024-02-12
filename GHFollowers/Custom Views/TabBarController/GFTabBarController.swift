f692851e56003631a55c8f3ff5905ba7d6eb8817
Billy Chan <billycychan.work@gmail.com>
2024-02-12 17:32:45 -0500

    refactor: extract tabbat related code to GFTabBarController

---
+++ GHFollowers/Custom Views/TabBarController/GFTabBarController.swift
@@ -0,0 +1,34 @@
+//
+//  GFTabBarController.swift
+//  GHFollowers
+//
+//  Created by CHI YU CHAN on 12/2/2024.
+//
+
+import UIKit
+
+class GFTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        viewControllers = [createSearchNavigationController(), createFavoritesNavigationController()]
+        UITabBar.appearance().tintColor = .systemGreen
+    }
+    
+    
+    func createSearchNavigationController() -> UINavigationController {
+        let searchVC = SearchVC()
+        searchVC.title = "Search"
+        searchVC.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 0)
+        
+        return UINavigationController(rootViewController: searchVC)
+    }
+    
+    func createFavoritesNavigationController() -> UINavigationController {
+        let favoriteListVC = FavoritesListVC()
+        favoriteListVC.title = "Favorite"
+        favoriteListVC.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
+        
+        return UINavigationController(rootViewController: favoriteListVC)
+    }
+}
--- GHFollowers/Support/SceneDelegate.swift
+++ GHFollowers/Support/SceneDelegate.swift
@@ -17,39 +17,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
 
         configureTabBar()
-        
+        configureNavigationBar()
+
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
-        window?.rootViewController = createTabbar()
+        window?.rootViewController = GFTabBarController()
         window?.makeKeyAndVisible()
-        
-        configureNavigationBar()
-        
-    }
-    
-    func createSearchNavigationController() -> UINavigationController {
-        let searchVC = SearchVC()
-        searchVC.title = "Search"
-        searchVC.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 0)
-        
-        return UINavigationController(rootViewController: searchVC)
-    }
-    
-    func createFavoritesNavigationController() -> UINavigationController {
-        let favoriteListVC = FavoritesListVC()
-        favoriteListVC.title = "Favorite"
-        favoriteListVC.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
-        
-        return UINavigationController(rootViewController: favoriteListVC)
-    }
-    
-    func createTabbar() -> UITabBarController {
-        let tabbar = UITabBarController()
-        UITabBar.appearance().tintColor = .systemGreen
-        tabbar.viewControllers = [createSearchNavigationController(),
-                                  createFavoritesNavigationController()]
-        return tabbar
-        
     }
     
     func configureNavigationBar() {
