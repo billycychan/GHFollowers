03a79ea5a84244d541112a0997838302a32ba36d
Billy Chan <billycychan.work@gmail.com>
2024-02-13 22:58:30 -0500

    refactor(iOS16): add SwiftUI View for FollowerView

---
+++ GHFollowers/Custom Views/Cells/FollowerCell/FollowerView.swift
@@ -0,0 +1,35 @@
+//
+//  FollowerView.swift
+//  GHFollowers
+//
+//  Created by CHI YU CHAN on 13/2/2024.
+//
+
+import SwiftUI
+
+struct FollowerView: View {
+    
+    var follower: Follower
+    
+    var body: some View {
+        VStack {
+            AsyncImage(url: URL(string: follower.avatarUrl)) { image in
+                image
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            } placeholder: {
+                Image("avatar-placeholder")
+            }
+            .clipShape(Circle())
+            
+            Text(follower.login)
+                .bold()
+                .lineLimit(1)
+                .minimumScaleFactor(0.6)
+        }
+    }
+}
+
+#Preview {
+    FollowerView(follower: Follower(login: "billycychan", avatarUrl: ""))
+}
